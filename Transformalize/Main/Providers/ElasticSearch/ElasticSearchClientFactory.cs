157c06642635d65e6bcf52a60f37a06eef2f9819
Dale Newman <dalenewman@gmail.com>
2014-09-26 16:23:56 -0400

    Fixed logging bug.

--- Transformalize/Main/Providers/ElasticSearch/ElasticSearchClientFactory.cs
+++ Transformalize/Main/Providers/ElasticSearch/ElasticSearchClientFactory.cs
@@ -9,26 +9,37 @@ namespace Transformalize.Main.Providers.ElasticSearch {
     public static class ElasticSearchClientFactory {
 
         public static ElasticSearchNetClient Create(AbstractConnection connection, Entity entity) {
-            TflLogger.Debug(entity.ProcessName, entity.Name, "Preparing Elasticsearch.NET client for {0}", connection.Uri());
+            var entityExists = entity != null;
+            var processName = entityExists ? entity.ProcessName : string.Empty;
+            var entityName = entityExists ? entity.Name : string.Empty;
+            var alias = entityExists ? entity.Alias.ToLower() : string.Empty;
+
+            TflLogger.Debug(processName, entityName, "Preparing Elasticsearch.NET client for {0}", connection.Uri());
             var pool = new SingleNodeConnectionPool(connection.Uri());
             var settings = new ConnectionConfiguration(pool);
 
             return new ElasticSearchNetClient(
                 new ElasticsearchClient(settings),
-                entity == null ? string.Empty : entity.ProcessName.ToLower(),
-                entity == null ? string.Empty : entity.Alias.ToLower()
+                processName.ToLower(),
+                alias
             );
         }
 
         public static NestClient CreateNest(AbstractConnection connection, Entity entity) {
-            TflLogger.Debug(entity.ProcessName, entity.Name, "Preparing NEST client for {0}", connection.Uri());
+
+            var entityExists = entity != null;
+            var processName = entityExists ? entity.ProcessName : string.Empty;
+            var entityName = entityExists ? entity.Name : string.Empty;
+            var alias = entityExists ? entity.Alias.ToLower() : string.Empty;
+
+            TflLogger.Debug(processName, entityName, "Preparing NEST client for {0}", connection.Uri());
             var pool = new SingleNodeConnectionPool(connection.Uri());
             var settings = new ConnectionSettings(pool);
 
             return new NestClient(
                 new ElasticClient(settings),
-                entity == null ? string.Empty : entity.ProcessName.ToLower(),
-                entity == null ? string.Empty : entity.Alias.ToLower()
+                processName.ToLower(),
+                alias
                 );
         }
 
--- Transformalize/Main/Template/TemplateActionRun.cs
+++ Transformalize/Main/Template/TemplateActionRun.cs
@@ -29,28 +29,40 @@ namespace Transformalize.Main {
 
         public override void Handle(TemplateAction action) {
 
+            if (action.Connection == null) {
+                TflLogger.Warn(action.ProcessName, string.Empty, "No connection provided for {0}", action.Action);
+                return;
+            }
+
+            if (!string.IsNullOrEmpty(action.Command)) {
+                var response = action.Connection.ExecuteScript(action.Command, action.Timeout);
+                if (response.Success) {
+                    TflLogger.Info(action.ProcessName, string.Empty, "Command: {0} ran ok.", action.Command);
+                    TflLogger.Debug(action.ProcessName, string.Empty, "Command: {0} affected {1} rows.", action.Command, response.RowsAffected < 0 ? 0 : response.RowsAffected);
+                } else {
+                    TflLogger.Error(action.ProcessName, string.Empty, "Failed to run command: {0}. {1} Error{2}.", action.Command, response.Messages.Count, response.Messages.Count.Plural());
+                    foreach (var message in response.Messages) {
+                        TflLogger.Error(action.ProcessName, string.Empty, message);
+                    }
+                }
+                return;
+            }
+
             var isTemplate = string.IsNullOrEmpty(action.File);
+
             var file = isTemplate ? action.RenderedFile : action.File;
 
-            if (string.IsNullOrEmpty(file)) {
+            if (string.IsNullOrEmpty(file) && string.IsNullOrEmpty(action.Command)) {
                 TflLogger.Warn(action.ProcessName, string.Empty, "Skipping run action.  It needs a template file, OR an action file attribute set.  Note: the action file superceeds the template file.");
                 return;
             }
 
             var fileInfo = new FileInfo(file);
+            var fileExists = fileInfo.Exists;
 
-            if (fileInfo.Exists) {
+            if (fileExists) {
                 var script = File.ReadAllText(fileInfo.FullName);
                 if (!string.IsNullOrEmpty(script)) {
-
-                    if (action.Connection == null) {
-                        if (isTemplate) {
-                            TflLogger.Warn(action.ProcessName, string.Empty, "Could not run {0} action for {1} template.  No connection provided.", action.Action, action.TemplateName);
-                        } else {
-                            TflLogger.Warn(action.ProcessName, string.Empty, "Could not run {0} action.  No connection provided.", action.Action);
-                        }
-                        return;
-                    }
 
                     var response = action.Connection.ExecuteScript(script, action.Timeout);
                     if (response.Success) {
@@ -66,11 +78,12 @@ namespace Transformalize.Main {
                     TflLogger.Warn(action.ProcessName, string.Empty, "{0} is empty.", fileInfo.Name);
                 }
             } else {
-                if(isTemplate)
+                if (isTemplate)
                     TflLogger.Warn(action.ProcessName, string.Empty, "file rendered output from {0} is not available.  It will not run.", action.TemplateName);
                 else
                     TflLogger.Warn(action.ProcessName, string.Empty, "file {0} output is not available.  It will not run.", fileInfo.Name);
             }
         }
+
     }
 }
