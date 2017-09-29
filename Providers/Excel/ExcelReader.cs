55220be467c66d99cc5d8069f452706d6bb1984a
Dale Newman <dalenewman@gmail.com>
2017-09-29 18:00:59 -0400

    Minor change to excel provider

--- Providers/Excel/ExcelReader.cs
+++ Providers/Excel/ExcelReader.cs
@@ -23,51 +23,42 @@ using Transformalize.Context;
 using Transformalize.Contracts;
 using Transformalize.Extensions;
 
-namespace Transformalize.Providers.Excel
-{
-    public class ExcelReader : IRead
-    {
+namespace Transformalize.Providers.Excel {
+    public class ExcelReader : IRead {
         private readonly InputContext _context;
         private readonly IRowFactory _rowFactory;
         private readonly FileInfo _fileInfo;
 
-        public ExcelReader(InputContext context, IRowFactory rowFactory)
-        {
+        public ExcelReader(InputContext context, IRowFactory rowFactory) {
             _fileInfo = new FileInfo(context.Connection.File);
             _context = context;
             _rowFactory = rowFactory;
         }
 
-        public IEnumerable<IRow> Read()
-        {
+        public IEnumerable<IRow> Read() {
 
             // TODO: fix major duplication here
 
-            using (var fileStream = File.Open(_fileInfo.FullName, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
-            {
+            using (var fileStream = File.Open(_fileInfo.FullName, FileMode.Open, FileAccess.Read, FileShare.ReadWrite)) {
                 var isBinary = _fileInfo.Extension.ToLower() == ".xls";
                 using (
                     var reader = isBinary
                         ? ExcelReaderFactory.CreateBinaryReader(fileStream)
-                        : ExcelReaderFactory.CreateOpenXmlReader(fileStream))
-                {
+                        : ExcelReaderFactory.CreateOpenXmlReader(fileStream)) {
 
                     int index = 0;
-                    if (reader == null)
-                    {
+                    if (reader == null) {
                         yield break;
                     }
 
                     var start = _context.Connection.Start;
                     var end = 0;
-                    if (_context.Entity.IsPageRequest())
-                    {
-                        start += (_context.Entity.Page*_context.Entity.PageSize) - _context.Entity.PageSize;
+                    if (_context.Entity.IsPageRequest()) {
+                        start += (_context.Entity.Page * _context.Entity.PageSize) - _context.Entity.PageSize;
                         end = start + _context.Entity.PageSize;
                     }
 
-                    for (var i = 0; i < start; i++)
-                    {
+                    for (var i = 0; i < start; i++) {
                         reader.Read();
                         ++index;
                     }
@@ -75,48 +66,33 @@ namespace Transformalize.Providers.Excel
                     var readerHasData = false;
                     var emptyDetector = new StringBuilder();
 
-                    if (reader.Read())
-                    {
+                    if (reader.Read()) {
                         ++index;
                         readerHasData = true;
 
-                        if (end > 0 && end <= index)
-                        {
+                        if (end > 0 && end <= index) {
                             _context.Entity.Hits++;
-                        }
-                        else
-                        {
+                        } else {
                             emptyDetector.Clear();
                             var row = _rowFactory.Create();
-                            for (var i = 0; i < _context.InputFields.Length; i++)
-                            {
+                            for (var i = 0; i < _context.InputFields.Length; i++) {
                                 var field = _context.InputFields[i];
 
                                 var expected = Constants.TypeSystem()[field.Type];
                                 var actual = reader.IsDBNull(i) ? null : reader.GetValue(i);
 
-                                if (_context.Entity.DataTypeWarnings)
-                                {
-                                    if (actual != null && expected != actual.GetType())
-                                    {
+                                if (_context.Entity.DataTypeWarnings) {
+                                    if (actual != null && expected != actual.GetType()) {
                                         _context.Warn(
                                             $"The {field.Alias} field in {_context.Entity.Alias} expects a {expected}, but is reading a ({actual.GetType().Name}){actual}.");
                                     }
                                 }
 
-                                if (field.Type == "string")
-                                {
-                                    row[field] = reader.IsDBNull(i) ? null : reader.GetValue(i).ToString();
-                                }
-                                else
-                                {
-                                    row[field] = reader.IsDBNull(i) ? null : field.Convert(reader.GetValue(i));
-                                }
+                                row[field] = reader.IsDBNull(i) ? null : field.Convert(reader.GetValue(i));
                                 emptyDetector.Append(row[field]);
                             }
                             emptyDetector.Trim(" ");
-                            if (!emptyDetector.ToString().Equals(string.Empty))
-                            {
+                            if (!emptyDetector.ToString().Equals(string.Empty)) {
                                 _context.Entity.Hits++;
                                 yield return row;
                             }
@@ -125,37 +101,23 @@ namespace Transformalize.Providers.Excel
 
                     }
 
-                    if (readerHasData)
-                    {
+                    if (readerHasData) {
 
-                        while (reader.Read())
-                        {
+                        while (reader.Read()) {
 
                             ++index;
-                            if (end > 0 && end <= index)
-                            {
+                            if (end > 0 && end <= index) {
                                 _context.Entity.Hits++;
-                            }
-                            else
-                            {
+                            } else {
                                 emptyDetector.Clear();
                                 var row = _rowFactory.Create();
-                                for (var i = 0; i < _context.InputFields.Length; i++)
-                                {
+                                for (var i = 0; i < _context.InputFields.Length; i++) {
                                     var field = _context.InputFields[i];
-                                    if (field.Type == "string")
-                                    {
-                                        row[field] = reader.IsDBNull(i) ? null : reader.GetString(i);
-                                    }
-                                    else
-                                    {
-                                        row[field] = reader.IsDBNull(i) ? null : field.Convert(reader.GetValue(i));
-                                    }
+                                    row[field] = reader.IsDBNull(i) ? null : field.Convert(reader.GetValue(i));
                                     emptyDetector.Append(row[field]);
                                 }
                                 emptyDetector.Trim(" ");
-                                if (!emptyDetector.ToString().Equals(string.Empty))
-                                {
+                                if (!emptyDetector.ToString().Equals(string.Empty)) {
                                     _context.Entity.Hits++;
                                     yield return row;
                                 }
