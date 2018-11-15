d32bd7120b965142b4e3d9920fb737aac9e080e2
Dale Newman <dalenewman@gmail.com>
2018-11-14 22:53:26 -0500

    update nuget

--- nuget.bat
+++ nuget.bat
@@ -1,10 +1,10 @@
-REM nuget pack Compatibility\Transformalize\Transformalize.nuspec -OutputDirectory "c:\temp\modules"
+nuget pack Compatibility\Transformalize\Transformalize.nuspec -OutputDirectory "c:\temp\modules"
 
-REM nuget pack Pipeline.Logging.NLog\Transformalize.Logging.NLog.nuspec -OutputDirectory "c:\temp\modules"
-REM nuget pack Pipeline.Scheduler.Quartz\Transformalize.Scheduler.Quartz.nuspec -OutputDirectory "c:\temp\modules"
+rem nuget pack Pipeline.Logging.NLog\Transformalize.Logging.NLog.nuspec -OutputDirectory "c:\temp\modules"
+rem nuget pack Pipeline.Scheduler.Quartz\Transformalize.Scheduler.Quartz.nuspec -OutputDirectory "c:\temp\modules"
 
-REM nuget pack Providers\Ado\Transformalize.Provider.Ado.nuspec -OutputDirectory "c:\temp\modules"
-nuget pack Providers\OpenXml\Transformalize.Provider.OpenXml.nuspec -OutputDirectory "c:\temp\modules"
+rem nuget pack Providers\Ado\Transformalize.Provider.Ado.nuspec -OutputDirectory "c:\temp\modules"
+rem nuget pack Providers\OpenXml\Transformalize.Provider.OpenXml.nuspec -OutputDirectory "c:\temp\modules"
 rem nuget pack Providers\File\Transformalize.Provider.File.nuspec -OutputDirectory "c:\temp\modules"
 rem nuget pack Providers\Web\Transformalize.Provider.Web.nuspec -OutputDirectory "c:\temp\modules"
 rem nuget pack Providers\Console\Transformalize.Provider.Console.nuspec -OutputDirectory "c:\temp\modules"
--- nuget.push.bat
+++ nuget.push.bat
@@ -1,12 +1,12 @@
 cd c:\code\Transformalize
-REM nuget push "c:\temp\modules\Transformalize.0.3.12-beta.nupkg" -source https://api.nuget.org/v3/index.json
+nuget push "c:\temp\modules\Transformalize.0.3.13-beta.nupkg" -source https://api.nuget.org/v3/index.json
 
 REM nuget push "c:\temp\modules\Transformalize.Logging.NLog.0.3.1-beta.nupkg" -source https://api.nuget.org/v3/index.json
 REM nuget push "c:\temp\modules\Transformalize.Provider.Trace.0.3.1-beta.nupkg" -source https://api.nuget.org/v3/index.json
 REM nuget push "c:\temp\modules\Transformalize.Provider.Console.0.3.1-beta.nupkg" -source https://api.nuget.org/v3/index.json
 
 REM nuget push "c:\temp\modules\Transformalize.Provider.Ado.0.3.11-beta.nupkg" -source https://api.nuget.org/v3/index.json
-nuget push "c:\temp\modules\Transformalize.Provider.OpenXml.0.3.12-beta.nupkg" -source https://api.nuget.org/v3/index.json
+REM nuget push "c:\temp\modules\Transformalize.Provider.OpenXml.0.3.12-beta.nupkg" -source https://api.nuget.org/v3/index.json
 REM nuget push "c:\temp\modules\Transformalize.Provider.File.0.2.10-beta.nupkg" -source https://api.nuget.org/v3/index.json
 
 REM nuget push "c:\temp\modules\Transformalize.Provider.SSAS.0.2.10-beta.nupkg" -source https://api.nuget.org/v3/index.json
