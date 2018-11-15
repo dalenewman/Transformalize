54f4e563b623e4195c4d568a92cdc28e0cf854f4
Dale Newman <dalenewman@gmail.com>
2018-11-14 23:38:36 -0500

    house-keeping for nuget stuff

---
+++ Nuget/nuget.bat
@@ -0,0 +1,18 @@
+REM nuget pack Transformalize.nuspec -OutputDirectory "c:\temp\modules"
+
+rem nuget pack Transformalize.Logging.NLog.nuspec -OutputDirectory "c:\temp\modules"
+rem nuget pack Transformalize.Scheduler.Quartz.nuspec -OutputDirectory "c:\temp\modules"
+
+rem nuget pack Transformalize.Provider.Ado.nuspec -OutputDirectory "c:\temp\modules"
+rem nuget pack Transformalize.Provider.OpenXml.nuspec -OutputDirectory "c:\temp\modules"
+rem nuget pack Transformalize.Provider.File.nuspec -OutputDirectory "c:\temp\modules"
+rem nuget pack Transformalize.Provider.GeoJson.nuspec -OutputDirectory "c:\temp\modules"
+nuget pack Transformalize.Provider.Kml.nuspec -OutputDirectory "c:\temp\modules"
+rem nuget pack Transformalize.Provider.Web.nuspec -OutputDirectory "c:\temp\modules"
+rem nuget pack Transformalize.Provider.Console.nuspec -OutputDirectory "c:\temp\modules"
+rem nuget pack Transformalize.Provider.Trace.nuspec -OutputDirectory "c:\temp\modules"
+
+rem nuget pack Transformalize.Transform.JavaScript.nuspec -OutputDirectory "c:\temp\modules"
+rem nuget pack Transformalize.Transform.Globalization.nuspec -OutputDirectory "c:\temp\modules"
+
+
