9542e611078dec8df04ac29902117d3e0d5570d8
Dale Newman <dalenewman@gmail.com>
2019-02-21 23:28:05 -0500

    fix messed up geojson package

--- Nuget/nuget.bat
+++ Nuget/nuget.bat
@@ -1,25 +1,25 @@
-nuget pack Transformalize.nuspec -OutputDirectory "c:\temp\modules"
-
-nuget pack Transformalize.Logging.NLog.nuspec -OutputDirectory "c:\temp\modules"
-nuget pack Transformalize.Scheduler.Quartz.nuspec -OutputDirectory "c:\temp\modules"
-
-rem PROVIDERS
-nuget pack Transformalize.Provider.OpenXml.nuspec -OutputDirectory "c:\temp\modules"
-nuget pack Transformalize.Provider.File.nuspec -OutputDirectory "c:\temp\modules"
+REM nuget pack Transformalize.nuspec -OutputDirectory "c:\temp\modules"
+REM 
+REM nuget pack Transformalize.Logging.NLog.nuspec -OutputDirectory "c:\temp\modules"
+REM nuget pack Transformalize.Scheduler.Quartz.nuspec -OutputDirectory "c:\temp\modules"
+REM 
+REM rem PROVIDERS
+REM nuget pack Transformalize.Provider.OpenXml.nuspec -OutputDirectory "c:\temp\modules"
+REM nuget pack Transformalize.Provider.File.nuspec -OutputDirectory "c:\temp\modules"
 nuget pack Transformalize.Provider.GeoJson.nuspec -OutputDirectory "c:\temp\modules"
-nuget pack Transformalize.Provider.Kml.nuspec -OutputDirectory "c:\temp\modules"
-nuget pack Transformalize.Provider.Web.nuspec -OutputDirectory "c:\temp\modules"
-nuget pack Transformalize.Provider.Console.nuspec -OutputDirectory "c:\temp\modules"
-nuget pack Transformalize.Provider.Trace.nuspec -OutputDirectory "c:\temp\modules"
-
-REM TRANSFORMS
-nuget pack Transformalize.Transform.JavaScript.nuspec -OutputDirectory "c:\temp\modules"
-nuget pack Transformalize.Transform.Globalization.nuspec -OutputDirectory "c:\temp\modules"
-nuget pack Transformalize.Transform.Compression.nuspec -OutputDirectory "c:\temp\modules"
-nuget pack Transformalize.Transform.DateMath.nuspec -OutputDirectory "c:\temp\modules"
-nuget pack Transformalize.Transform.Geography.nuspec -OutputDirectory "c:\temp\modules"
-nuget pack Transformalize.Transform.Html.nuspec -OutputDirectory "c:\temp\modules"
-nuget pack Transformalize.Transform.Json.nuspec -OutputDirectory "c:\temp\modules"
-nuget pack Transformalize.Transform.Xml.nuspec -OutputDirectory "c:\temp\modules"
+REM nuget pack Transformalize.Provider.Kml.nuspec -OutputDirectory "c:\temp\modules"
+REM nuget pack Transformalize.Provider.Web.nuspec -OutputDirectory "c:\temp\modules"
+REM nuget pack Transformalize.Provider.Console.nuspec -OutputDirectory "c:\temp\modules"
+REM nuget pack Transformalize.Provider.Trace.nuspec -OutputDirectory "c:\temp\modules"
+REM 
+REM REM TRANSFORMS
+REM nuget pack Transformalize.Transform.JavaScript.nuspec -OutputDirectory "c:\temp\modules"
+REM nuget pack Transformalize.Transform.Globalization.nuspec -OutputDirectory "c:\temp\modules"
+REM nuget pack Transformalize.Transform.Compression.nuspec -OutputDirectory "c:\temp\modules"
+REM nuget pack Transformalize.Transform.DateMath.nuspec -OutputDirectory "c:\temp\modules"
+REM nuget pack Transformalize.Transform.Geography.nuspec -OutputDirectory "c:\temp\modules"
+REM nuget pack Transformalize.Transform.Html.nuspec -OutputDirectory "c:\temp\modules"
+REM nuget pack Transformalize.Transform.Json.nuspec -OutputDirectory "c:\temp\modules"
+REM nuget pack Transformalize.Transform.Xml.nuspec -OutputDirectory "c:\temp\modules"
 
 
