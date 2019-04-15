e5220c58ec70be806d89dc1bbb2a8a5ddfe3105c
Dale Newman <dalenewman@gmail.com>
2019-04-15 08:49:12 -0400

    added active directory plugin

--- Nuget/nuget.push.bat
+++ Nuget/nuget.push.bat
@@ -1,17 +1,17 @@
 REM nuget push "c:\temp\modules\Transformalize.0.5.1-beta.nupkg" -source https://api.nuget.org/v3/index.json
-
+REM 
 REM nuget push "c:\temp\modules\Transformalize.Scheduler.Quartz.0.5.0-beta.nupkg" -source https://api.nuget.org/v3/index.json
 REM nuget push "c:\temp\modules\Transformalize.Logging.NLog.0.5.0-beta.nupkg" -source https://api.nuget.org/v3/index.json
-
-REM PROVIDERS
+REM 
+REM REM PROVIDERS
 REM nuget push "c:\temp\modules\Transformalize.Provider.Trace.0.5.0-beta.nupkg" -source https://api.nuget.org/v3/index.json
 REM nuget push "c:\temp\modules\Transformalize.Provider.Console.0.5.0-beta.nupkg" -source https://api.nuget.org/v3/index.json
 REM nuget push "c:\temp\modules\Transformalize.Provider.OpenXml.0.5.0-beta.nupkg" -source https://api.nuget.org/v3/index.json
 REM nuget push "c:\temp\modules\Transformalize.Provider.File.0.5.0-beta.nupkg" -source https://api.nuget.org/v3/index.json
 REM nuget push "c:\temp\modules\Transformalize.Provider.GeoJson.0.5.0-beta.nupkg" -source https://api.nuget.org/v3/index.json
 REM nuget push "c:\temp\modules\Transformalize.Provider.Kml.0.5.0-beta.nupkg" -source https://api.nuget.org/v3/index.json
-
-REM TRANSFORMS
+REM 
+REM REM TRANSFORMS
 REM nuget push "c:\temp\modules\Transformalize.Transform.Compression.0.5.0-beta.nupkg" -source https://api.nuget.org/v3/index.json
 REM nuget push "c:\temp\modules\Transformalize.Transform.DateMath.0.5.0-beta.nupkg" -source https://api.nuget.org/v3/index.json
 REM nuget push "c:\temp\modules\Transformalize.Transform.Globalization.0.5.0-beta.nupkg" -source https://api.nuget.org/v3/index.json
