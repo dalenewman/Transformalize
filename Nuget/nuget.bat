56fa11a0d190b2e99ccc5c439b0014215ca1f7e1
Dale Newman <dalenewman@gmail.com>
2020-03-31 13:51:34 -0400

    added .net standard 2.0 target to console provider

--- Nuget/nuget.bat
+++ Nuget/nuget.bat
@@ -7,7 +7,7 @@ REM rem PROVIDERS
 REM nuget pack Transformalize.Provider.OpenXml.nuspec -OutputDirectory "c:\temp\modules"
 REM nuget pack Transformalize.Provider.File.nuspec -OutputDirectory "c:\temp\modules"
 REM nuget pack Transformalize.Provider.Kml.nuspec -OutputDirectory "c:\temp\modules"
-REM nuget pack Transformalize.Provider.Console.nuspec -OutputDirectory "c:\temp\modules"
+nuget pack Transformalize.Provider.Console.nuspec -OutputDirectory "c:\temp\modules"
 REM nuget pack Transformalize.Provider.Trace.nuspec -OutputDirectory "c:\temp\modules"
 REM 
 REM REM TRANSFORMS
@@ -19,8 +19,8 @@ REM nuget pack Transformalize.Transform.Json.nuspec -OutputDirectory "c:\temp\mo
 REM nuget pack Transformalize.Transform.Xml.nuspec -OutputDirectory "c:\temp\modules"
 
 REM CONTAINERS
-nuget pack Transformalize.Container.Autofac.nuspec -OutputDirectory "c:\temp\modules"
-nuget pack Transformalize.Container.Autofac.v3.nuspec -OutputDirectory "c:\temp\modules"
+REM nuget pack Transformalize.Container.Autofac.nuspec -OutputDirectory "c:\temp\modules"
+REM nuget pack Transformalize.Container.Autofac.v3.nuspec -OutputDirectory "c:\temp\modules"
 
 
 
