524fe836b3dbaf92cdfea7aaceaee0c0377ebf6d
Dale Newman <dalenewman@gmail.com>
2020-08-04 16:33:47 -0400

    Update to Packages

--- Nuget/myget.push.bat
+++ Nuget/myget.push.bat
@@ -1,13 +1,20 @@
-nuget push "c:\temp\modules\Transformalize.0.8.22-beta.nupkg" -source https://www.myget.org/F/transformalize/api/v3/index.json
+REM nuget push "c:\temp\modules\Transformalize.0.8.22-beta.nupkg" -source https://www.myget.org/F/transformalize/api/v3/index.json
+REM 
+REM nuget push "c:\temp\modules\Transformalize.Container.Autofac.0.8.23-beta.nupkg" -source https://www.myget.org/F/transformalize/api/v3/index.json
+REM nuget push "c:\temp\modules\Transformalize.Container.Autofac.v3.0.8.23-beta.nupkg" -source https://www.myget.org/F/transformalize/api/v3/index.json
+REM 
+REM nuget push "c:\temp\modules\Transformalize.Provider.Console.0.8.22-beta.nupkg" -source https://www.myget.org/F/transformalize/api/v3/index.json
+REM nuget push "c:\temp\modules\Transformalize.Provider.Console.Autofac.0.8.22-beta.nupkg" -source https://www.myget.org/F/transformalize/api/v3/index.json
 
-nuget push "c:\temp\modules\Transformalize.Container.Autofac.0.8.23-beta.nupkg" -source https://www.myget.org/F/transformalize/api/v3/index.json
-nuget push "c:\temp\modules\Transformalize.Container.Autofac.v3.0.8.23-beta.nupkg" -source https://www.myget.org/F/transformalize/api/v3/index.json
-
-nuget push "c:\temp\modules\Transformalize.Provider.Console.0.8.22-beta.nupkg" -source https://www.myget.org/F/transformalize/api/v3/index.json
-nuget push "c:\temp\modules\Transformalize.Provider.Console.Autofac.0.8.22-beta.nupkg" -source https://www.myget.org/F/transformalize/api/v3/index.json
-
-REM nuget push "c:\temp\modules\Transformalize.Provider.File.0.8.14-beta.nupkg" -source https://www.myget.org/F/transformalize/api/v3/index.json
-REM nuget push "c:\temp\modules\Transformalize.Provider.File.Autofac.0.8.14-beta.nupkg" -source https://www.myget.org/F/transformalize/api/v3/index.json
-
-REM nuget push "c:\temp\modules\Transformalize.Provider.Trace.0.8.3-beta.nupkg" -source https://www.myget.org/F/transformalize/api/v3/index.json
+REM nuget push "c:\temp\modules\Transformalize.Provider.File.0.8.22-beta.nupkg" -source https://www.myget.org/F/transformalize/api/v3/index.json
+REM nuget push "c:\temp\modules\Transformalize.Provider.File.Autofac.0.8.22-beta.nupkg" -source https://www.myget.org/F/transformalize/api/v3/index.json
+REM 
+REM nuget push "c:\temp\modules\Transformalize.Provider.Trace.0.8.22-beta.nupkg" -source https://www.myget.org/F/transformalize/api/v3/index.json
+REM nuget push "c:\temp\modules\Transformalize.Provider.OpenXml.0.8.22-beta.nupkg" -source https://www.myget.org/F/transformalize/api/v3/index.json
+REM nuget push "c:\temp\modules\Transformalize.Provider.Kml.0.8.22-beta.nupkg" -source https://www.myget.org/F/transformalize/api/v3/index.json
+REM 
+nuget push "c:\temp\modules\Transformalize.Transform.Compression.0.8.22-beta.nupkg" -source https://www.myget.org/F/transformalize/api/v3/index.json
+nuget push "c:\temp\modules\Transformalize.Transform.Globalization.0.8.22-beta.nupkg" -source https://www.myget.org/F/transformalize/api/v3/index.json
+nuget push "c:\temp\modules\Transformalize.Transform.Geography.0.8.22-beta.nupkg" -source https://www.myget.org/F/transformalize/api/v3/index.json
+nuget push "c:\temp\modules\Transformalize.Transform.Xml.0.8.22-beta.nupkg" -source https://www.myget.org/F/transformalize/api/v3/index.json
 
