b196b0bd74149b5e70321fb76b7ddbede12dc6e9
Dale Newman <dalenewman@gmail.com>
2020-12-05 18:46:27 -0500

    .NET Core CLI WIP
    - added SQLite provider
    - added Elasticsearch provider
    - added Jint transform

--- src/CLI/Program.cs
+++ src/CLI/Program.cs
@@ -5,15 +5,18 @@ using System.Collections.Generic;
 using System.Linq;
 using Transformalize.Configuration;
 using Transformalize.Containers.Autofac;
-using Transformalize.Containers.Autofac.Modules;
 using Transformalize.Contracts;
+using Transformalize.Providers.Ado.Autofac;
 using Transformalize.Providers.Bogus.Autofac;
 using Transformalize.Providers.Console;
 using Transformalize.Providers.Console.Autofac;
 using Transformalize.Providers.CsvHelper.Autofac;
+using Transformalize.Providers.Elasticsearch.Autofac;
 using Transformalize.Providers.Json.Autofac;
 using Transformalize.Providers.PostgreSql.Autofac;
+using Transformalize.Providers.Sqlite.Autofac;
 using Transformalize.Providers.SqlServer.Autofac;
+using Transformalize.Transforms.Jint.Autofac;
 
 namespace Transformalize.Cli {
    class Program {
@@ -30,7 +33,12 @@ namespace Transformalize.Cli {
 
          var logger = new ConsoleLogger(options.LogLevel);
 
-         using (var outer = new ConfigurationContainer().CreateScope(options.ArrangementWithMode(), logger, options.GetParameters())) {
+         var operations = new List<Autofac.Core.IModule> {
+            new JintTransformModule()
+         };
+         // razor, lambda parser, fluid, humanizer, etc
+
+         using (var outer = new ConfigurationContainer(operations.ToArray()).CreateScope(options.ArrangementWithMode(), logger, options.GetParameters())) {
 
             var process = outer.Resolve<Process>();
 
@@ -38,46 +46,52 @@ namespace Transformalize.Cli {
                Environment.Exit(1);
             }
 
-            var modules = new List<Autofac.Core.IModule> {
+            var providers = new List<Autofac.Core.IModule> {
                new ConsoleProviderModule()
             };
 
             var output = process.GetOutputConnection();
 
-            if(output == null || output.Provider == "internal" || output.Provider == "console") {
+            if (output == null || output.Provider == "internal" || output.Provider == "console") {
                logger.SuppressConsole();
-               if(options.Format == "csv") {
+               if (options.Format == "csv") {
                   output.Provider = "file";  // delimited file
                   output.Delimiter = ",";
                   output.Stream = true;
                   output.File = "dummy.csv";
-                  modules.Add(new CsvHelperProviderModule(System.Console.OpenStandardOutput()));
+                  providers.Add(new CsvHelperProviderModule(Console.OpenStandardOutput()));
                } else {
                   output.Provider = "json";
                   output.Stream = true;
                   output.Format = "json";
                   output.File = "dummy.json";
-                  modules.Add(new JsonProviderModule(System.Console.OpenStandardOutput()));
+                  providers.Add(new JsonProviderModule(Console.OpenStandardOutput()));
                }
             } else {
-               modules.Add(new CsvHelperProviderModule());
-               modules.Add(new JsonProviderModule());
+               providers.Add(new CsvHelperProviderModule());
+               providers.Add(new JsonProviderModule());
             }
 
-            var providers = new HashSet<string>(process.Connections.Select(c => c.Provider));
+            // PROVIDERS
+            providers.Add(new AdoProviderModule());
+            providers.Add(new BogusModule());
+            providers.Add(new SqliteModule());
+            providers.Add(new SqlServerModule());
+            providers.Add(new PostgreSqlModule());
+            // mysql
+            providers.Add(new ElasticsearchModule());
+            // solr
+            // razor (templates)
 
-            if (providers.Contains("sqlserver")) { modules.Add(new SqlServerModule()); }
-            if (providers.Contains("postgresql")) { modules.Add(new PostgreSqlModule()); }
-            if (providers.Contains("bogus")) { modules.Add(new BogusModule()); }
+            var modules = providers.Union(operations).ToArray();
 
-            using (var inner = new Container(modules.ToArray()).CreateScope(process, logger)) {
-               var controller = inner.Resolve<IProcessController>();
-               controller.Execute();
+            using (var inner = new Container(modules).CreateScope(process, logger)) {
+               inner.Resolve<IProcessController>().Execute();
             }
          }
       }
       static void CommandLineError(IEnumerable<Error> errors) {
-         System.Environment.Exit(1);
+         Environment.Exit(1);
       }
 
 
