180430846cc57529f8601a785418c199c5d5ab48
Dale Newman <dalenewman@gmail.com>
2022-01-30 15:01:15 -0500

    0.8.43 (and accidently 0.8.23) as a version mishap - corrected to 0.8.44

--- Compatibility/Transformalize.452/Properties/AssemblyInfo.cs
+++ Compatibility/Transformalize.452/Properties/AssemblyInfo.cs
@@ -28,7 +28,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Transformalize.452")]
-[assembly: AssemblyCopyright("Copyright © 2013-2020")]
+[assembly: AssemblyCopyright("Copyright © 2013-2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -50,7 +50,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.8.23.0")]
-[assembly: AssemblyFileVersion("0.8.23.0")]
+[assembly: AssemblyVersion("0.8.44.0")]
+[assembly: AssemblyFileVersion("0.8.44.0")]
 [assembly: NeutralResourcesLanguage("en-US")]
 
--- Nuget/myget.push.bat
+++ Nuget/myget.push.bat
@@ -1,4 +1,4 @@
-nuget push "c:\temp\modules\Transformalize.0.8.43-beta.nupkg" -source https://www.myget.org/F/transformalize/api/v3/index.json
+nuget push "c:\temp\modules\Transformalize.0.8.44-beta.nupkg" -source https://www.myget.org/F/transformalize/api/v3/index.json
 REM 
 REM nuget push "c:\temp\modules\Transformalize.Container.Autofac.0.8.41-beta.nupkg" -source https://www.myget.org/F/transformalize/api/v3/index.json
 REM nuget push "c:\temp\modules\Transformalize.Container.Autofac.v3.0.8.41-beta.nupkg" -source https://www.myget.org/F/transformalize/api/v3/index.json
