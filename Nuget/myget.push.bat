b609f3d2d8b41d3bb4042be2b6602fca8731e1af
Dale Newman <dalenewman@gmail.com>
2025-04-22 13:46:47 -0400

    update these versions too

--- Nuget/myget.push.bat
+++ Nuget/myget.push.bat
@@ -1,23 +1,23 @@
 REM Main
-nuget push "c:\temp\modules\Transformalize.0.11.3-beta.nupkg" -source https://www.myget.org/F/transformalize/api/v3/index.json
+REM nuget push "c:\temp\modules\Transformalize.0.11.3-beta.nupkg" -source https://www.myget.org/F/transformalize/api/v3/index.json
 
 REM Containers 
-REM nuget push "c:\temp\modules\Transformalize.Container.Autofac.0.11.1-beta.nupkg" -source https://www.myget.org/F/transformalize/api/v3/index.json
+REM nuget push "c:\temp\modules\Transformalize.Container.Autofac.0.11.3-beta.nupkg" -source https://www.myget.org/F/transformalize/api/v3/index.json
 
 REM Providers
-REM nuget push "c:\temp\modules\Transformalize.Provider.Console.0.11.1-beta.nupkg" -source https://www.myget.org/F/transformalize/api/v3/index.json
-REM nuget push "c:\temp\modules\Transformalize.Provider.Console.Autofac.0.11.1-beta.nupkg" -source https://www.myget.org/F/transformalize/api/v3/index.json
-REM nuget push "c:\temp\modules\Transformalize.Provider.File.0.11.1-beta.nupkg" -source https://www.myget.org/F/transformalize/api/v3/index.json
-REM nuget push "c:\temp\modules\Transformalize.Provider.File.Autofac.0.11.1-beta.nupkg" -source https://www.myget.org/F/transformalize/api/v3/index.json
-REM nuget push "c:\temp\modules\Transformalize.Provider.Trace.0.11.1-beta.nupkg" -source https://www.myget.org/F/transformalize/api/v3/index.json
-REM nuget push "c:\temp\modules\Transformalize.Provider.OpenXml.0.11.1-beta.nupkg" -source https://www.myget.org/F/transformalize/api/v3/index.json
-REM nuget push "c:\temp\modules\Transformalize.Provider.Kml.0.11.1-beta.nupkg" -source https://www.myget.org/F/transformalize/api/v3/index.json
+REM nuget push "c:\temp\modules\Transformalize.Provider.Console.0.11.3-beta.nupkg" -source https://www.myget.org/F/transformalize/api/v3/index.json
+REM nuget push "c:\temp\modules\Transformalize.Provider.Console.Autofac.0.11.3-beta.nupkg" -source https://www.myget.org/F/transformalize/api/v3/index.json
+REM nuget push "c:\temp\modules\Transformalize.Provider.File.0.11.3-beta.nupkg" -source https://www.myget.org/F/transformalize/api/v3/index.json
+REM nuget push "c:\temp\modules\Transformalize.Provider.File.Autofac.0.11.3-beta.nupkg" -source https://www.myget.org/F/transformalize/api/v3/index.json
+REM nuget push "c:\temp\modules\Transformalize.Provider.Trace.0.11.3-beta.nupkg" -source https://www.myget.org/F/transformalize/api/v3/index.json
+REM nuget push "c:\temp\modules\Transformalize.Provider.OpenXml.0.11.3-beta.nupkg" -source https://www.myget.org/F/transformalize/api/v3/index.json
+REM nuget push "c:\temp\modules\Transformalize.Provider.Kml.0.11.3-beta.nupkg" -source https://www.myget.org/F/transformalize/api/v3/index.json
 
 REM Transforms
-REM nuget push "c:\temp\modules\Transformalize.Transform.Compression.0.11.1-beta.nupkg" -source https://www.myget.org/F/transformalize/api/v3/index.json
-REM nuget push "c:\temp\modules\Transformalize.Transform.Globalization.0.11.1-beta.nupkg" -source https://www.myget.org/F/transformalize/api/v3/index.json
-REM nuget push "c:\temp\modules\Transformalize.Transform.Geography.0.11.1-beta.nupkg" -source https://www.myget.org/F/transformalize/api/v3/index.json
-REM nuget push "c:\temp\modules\Transformalize.Transform.Xml.0.11.1-beta.nupkg" -source https://www.myget.org/F/transformalize/api/v3/index.jREM son
+REM nuget push "c:\temp\modules\Transformalize.Transform.Compression.0.11.3-beta.nupkg" -source https://www.myget.org/F/transformalize/api/v3/index.json
+REM nuget push "c:\temp\modules\Transformalize.Transform.Globalization.0.11.3-beta.nupkg" -source https://www.myget.org/F/transformalize/api/v3/index.json
+REM nuget push "c:\temp\modules\Transformalize.Transform.Geography.0.11.3-beta.nupkg" -source https://www.myget.org/F/transformalize/api/v3/index.json
+REM nuget push "c:\temp\modules\Transformalize.Transform.Xml.0.11.3-beta.nupkg" -source https://www.myget.org/F/transformalize/api/v3/index.json
 
 REM Misc
-REM nuget push "c:\temp\modules\Transformalize.Logging.NLog.0.11.1-beta.nupkg" -source https://www.myget.org/F/transformalize/api/v3/index.json
+REM nuget push "c:\temp\modules\Transformalize.Logging.NLog.0.11.3-beta.nupkg" -source https://www.myget.org/F/transformalize/api/v3/index.json
