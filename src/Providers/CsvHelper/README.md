d135a9862826162a419e25abede33d524120c2fe
dalenewman <dalenewman@gmail.com>
2026-02-21 20:38:01 -0500

    import csvhelper (#44)

---
+++ src/Providers/CsvHelper/README.md
@@ -0,0 +1,65 @@
+﻿This is a [Transformalize](https://github.com/dalenewman/Transformalize) file provider 
+using [CsvHelper](https://joshclose.github.io/CsvHelper/) to read/write files.
+
+### Write Usage
+
+```xml
+<add name='BogusWrite' read-only='true'>
+  <connections>
+    <add name='input' provider='bogus' seed='1' />
+    <add name='output' provider='file' file='c:\temp\bogus.csv' synchronous='true' />
+  </connections>
+  <entities>
+    <add name='Contact' size='1000'>
+      <fields>
+        <add name='Identity' type='int' />
+        <add name='FirstName' />
+        <add name='LastName' />
+        <add name='Stars' type='byte' min='1' max='5' />
+        <add name='Reviewers' type='int' min='0' max='500' />
+      </fields>
+    </add>
+  </entities>
+</add>
+```
+
+This writes 1000 rows of bogus data to a file. ⚠️ Presently you have to add `synchronous='true'`.
+
+### Read Usage
+
+```xml
+<add name='BogusRead' >
+  <connections>
+    <add name='input' provider='file' file='c:\temp\bogus.csv' synchronous='true' />
+  </connections>
+  <entities>
+    <add name='Bogus' page='1' size='10'>
+      <order>
+        <add field='Identity' />
+      </order>
+      <fields>
+        <add name='Identity' type='int' />
+        <add name='FirstName' />
+        <add name='LastName' />
+        <add name='Stars' type='byte' />
+        <add name='Reviewers' type='int' />
+      </fields>
+    </add>
+  </entities>
+</add>
+```
+
+This reads 10 rows of bogus data from a the file:
+
+<pre>
+<strong>Identity,FirstName,LastName,Stars,Reviewers</strong>
+1,Justin,Konopelski,3,153
+2,Eula,Schinner,2,41
+3,Tanya,Shanahan,4,412
+4,Emilio,Hand,4,469
+5,Rachel,Abshire,3,341
+6,Doyle,Beatty,4,458
+7,Delbert,Durgan,2,174
+8,Harold,Blanda,4,125
+9,Willie,Heaney,5,342
+10,Sophie,Hand,2,176</pre>
---
+++ src/Providers/CsvHelper/Transformalize.Provider.CsvHelper/CsvHelperStreamReader.cs
@@ -0,0 +1,97 @@
+﻿using CsvHelper;
+using CsvHelper.Configuration;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using Transformalize.Configuration;
+using Transformalize.Context;
+using Transformalize.Contracts;
+using Transformalize.Extensions;
+using Transformalize.Transforms;
+
+namespace Transformalize.Providers.CsvHelper {
+
+   public class CsvHelperStreamReader : IRead {
+
+      private readonly InputContext _context;
+      private readonly IRowFactory _rowFactory;
+      private readonly List<ITransform> _transforms = new List<ITransform>();
+      private readonly StreamReader _streamReader;
+
+      public CsvHelperStreamReader(InputContext context, StreamReader streamReader, IRowFactory rowFactory) {
+         _context = context;
+         _streamReader = streamReader;
+         _rowFactory = rowFactory;
+
+         foreach (var field in context.Entity.Fields.Where(f => f.Input && f.Type != "string" && (!f.Transforms.Any() || f.Transforms.First().Method != "convert"))) {
+            _transforms.Add(new ConvertTransform(new PipelineContext(context.Logger, context.Process, context.Entity, field, new Operation { Method = "convert" })));
+         }
+      }
+
+      public IEnumerable<IRow> Read() {
+         return _transforms.Aggregate(PreRead(), (rows, transform) => transform.Operate(rows));
+      }
+
+      private IEnumerable<IRow> PreRead() {
+
+         _context.Debug(() => "Reading file stream.");
+
+         var ignoreFirstLines = _context.Connection.Start > 1 ? _context.Connection.Start - 1 : _context.Connection.Start;
+
+         var start = _context.Connection.Start;
+         var end = 0;
+         if (_context.Entity.IsPageRequest()) {
+            start += (_context.Entity.Page * _context.Entity.Size) - _context.Entity.Size;
+            end = start + _context.Entity.Size;
+         }
+
+         var current = _context.Connection.Start;
+
+         var configuration = new CsvConfiguration(CultureInfo.InvariantCulture) {
+            IgnoreBlankLines = true,
+            Delimiter = string.IsNullOrEmpty(_context.Connection.Delimiter) ? "," : _context.Connection.Delimiter,
+            Encoding = Encoding.GetEncoding(_context.Connection.Encoding)
+         };
+
+         if (_context.Connection.ErrorMode.Equals("IgnoreAndContinue", System.StringComparison.OrdinalIgnoreCase)) {
+            configuration.BadDataFound = null;  // skip the record
+         }
+
+         if (_context.Connection.TextQualifier != string.Empty) {
+            configuration.Escape = _context.Connection.TextQualifier[0];
+            configuration.Quote = _context.Connection.TextQualifier[0];
+         }
+
+         using (var csv = new CsvReader(_streamReader, configuration)) {
+
+            while (csv.Read()) {
+
+               if (csv.Parser.RawRow <= ignoreFirstLines) {
+                  continue;
+               }
+
+               if (end == 0 || current.Between(start, end)) {
+                  var row = _rowFactory.Create();
+                  for (int i = 0; i < _context.InputFields.Length; i++) {
+                     var data = csv.GetField(i);
+                     var field = _context.InputFields[i];
+                     row[field] = data;
+                  }
+                  yield return row;
+                  ++_context.Entity.Hits;
+               }
+               ++current;
+               if (current == end) {
+                  break;
+               }
+            }
+         }
+
+         _streamReader.Close();
+
+      }
+
+   }
+}
